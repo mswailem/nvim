f41b4d67522cc364aca84debf7db5e43664d1f29
mswailem <mswailem@mswailem>
2024-05-05 20:39:28 -0400

    Solved a quick bug that would happen when I open a non-python file

--- lua/settings.lua
+++ lua/settings.lua
@@ -38,6 +38,7 @@ vim.g.slime_default_config = {socket_name = "default", target_pane = "1"}
 vim.g.slime_dont_ask_default = 1
 vim.g.slime_bracketed_paste = 1
 vim.g.slime_no_mappings = 1
+vim.g.slime_cell_delimiter = ""
 
 -- Set cell delimiter for Python and IPython files
 vim.api.nvim_exec([[
